b25a52aed667dbe874b181f3c3ddf9179ff0abd9
Chris Rundell <cdrundell@icloud.com>
2022-01-15 14:58:52 +0000

    CRUD completed, style unfinished

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles
+
+  # get 'articles', to: 'articles#index', as: :articles
+  # get 'articles/new', to: 'articles#new', as: :new
+
+  # get 'articles/:id', to: 'articles#show', as: :article
+
+  # get 'articles/:id/edit', to: 'articles#edit', as: :edit
+  # patch 'articles/:id', to: 'articles#update'
+
+  # delete 'articles/:id', to: 'articles#destroy'
+
+  # post 'articles', to: 'articles#create'
 end
